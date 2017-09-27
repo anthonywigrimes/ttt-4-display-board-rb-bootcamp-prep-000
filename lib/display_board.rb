939e02ab5e8fd33519ddb3983e7c9c0aa7d0eb72
Anthony Grimes <agrimes@charter.net>
2017-09-27 17:33:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(options)
-
+    puts " #{options[0]} | #{options[1]} | #{options[2]} "
+    puts "--------------"
 end
