18d0d58d6603ceeeb709a657a0b47f2d5d480f2d
Anthony Grimes <agrimes@charter.net>
2017-09-27 17:34:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,8 @@
 # out the current state.
 def display_board(options)
     puts " #{options[0]} | #{options[1]} | #{options[2]} "
-    puts "--------------"
+    puts "-----------"
+    puts " #{options[3]} | #{options[4]} | #{options[5]} "
+    puts "-----------"
+    puts " #{options[6]} | #{options[7]} | #{options[8]} "
 end
